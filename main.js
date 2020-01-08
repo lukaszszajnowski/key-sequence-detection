cf204e9c2673d9b40fe9190c12cf4cd3b9db7a17
Łukasz Szajnowski <szajnowski@mailplus.pl>
2020-01-08 17:28:58 +0100

    Make the code field.

--- main.js
+++ main.js
@@ -2,14 +2,19 @@ const beepGood = new Audio("beep-good.ogg")
 const pressed = [];
 const secretCode = "fabi";
 
+const letter = document.querySelectorAll(".letter");
+
 window.addEventListener('keyup', (e) => {
     console.log(e);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     if (pressed.join('').includes(secretCode)) {
-        console.log("Dupa!")
         beepGood.currentTime = 0;
         beepGood.play();
     }
     console.log(pressed);
+    letter[0].textContent = pressed[0];
+    letter[1].textContent = pressed[1];
+    letter[2].textContent = pressed[2];
+    letter[3].textContent = pressed[3];
 })
